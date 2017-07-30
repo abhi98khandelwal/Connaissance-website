278311546ab8e5decb9edbb3ff0bc21013ea543f
Abhishek9900 <abhi.krpl100@gmail.com>
2017-07-30 11:42:25 +0530

    Mod

--- content/admin.py
+++ content/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Event
+from .models import PEvent, WEvent
 # Register your models here.
 
-admin.site.register(Event)
+admin.site.register(WEvent)
+admin.site.register(PEvent)
--- content/urls.py
+++ content/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     url(r'home/$', views.home, name='home'),
     url(r'events/$', views.events, name='events'),
-    url(r'events/eventp/$', views.eventsp, name='eventp'),
-    url(r'events/eventw/$', views.eventsw, name='eventw'),
+    url(r'events/eventp/$', views.eventp, name='eventp'),
+    url(r'events/eventw/$', views.eventw, name='eventw'),
     url(r'sponsors/$', views.sponsors, name='sponsors'),
     url(r'con16/$', views.con16, name='con16'),
 ]
