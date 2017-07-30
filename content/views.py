c0fa9129924030371759a846b5d70b9951bbf1f7
Abhishek9900 <abhi.krpl100@gmail.com>
2017-07-30 11:19:07 +0530

    Progress

--- content/views.py
+++ content/views.py
@@ -1,10 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404, redirect
-from django.conf import settings
+from django.shortcuts import render
 from . import forms
-from .models import PEvent, WEvent
-import os
 
 
 def home(request):
@@ -16,5 +13,23 @@ def sponsors(request):
 def con16(request):
     return render(request, 'content/con16.html')
 
+def events(request):
+    return render(request, 'content/events.html')
 
 def eventp(request):
+    form = forms.PhotoForm()
+    if request.method == 'POST':
+        form = forms.PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save()
+            return HttpResponseRedirect(reverse('content:events'))
+    return render(request, 'content/eventp.html', {'form':form})
+
+def eventw(request):
+    form = forms.WriteForm()
+    if request.method == 'POST':
+        form = forms.WriteForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save()
+            return HttpResponseRedirect(reverse('content:events'))
+    return render(request, 'content/eventw.html', {'form':form})
